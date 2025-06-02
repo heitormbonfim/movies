b52ae7cf9d11d7408252e0cb9e8fa05ba3c43397
heitormbonfim <heitor.mb27@gmail.com>
2025-06-02 20:47:32 -0300

    feat: add category service

---
+++ src/main/java/com/movies/service/CategoryService.java
@@ -0,0 +1,18 @@
+package com.movies.service;
+
+import com.movies.entity.Category;
+import com.movies.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+}
