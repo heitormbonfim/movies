3999596f8fa78f6af95c490b7045e3a1037e68ac
heitormbonfim <heitor.mb27@gmail.com>
2025-06-05 21:29:44 -0300

    refactor, feat: add DTOs and improved response from endpoints

--- src/main/java/com/movies/controller/CategoryController.java
+++ src/main/java/com/movies/controller/CategoryController.java
@@ -1,11 +1,17 @@
 package com.movies.controller;
 
+import com.movies.controller.request.CategoryRequest;
+import com.movies.controller.response.CategoryResponse;
 import com.movies.entity.Category;
+import com.movies.mapper.CategoryMapper;
 import com.movies.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movies/category")
@@ -15,22 +21,35 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping
-    public List<Category> getAllCategories() {
-        return categoryService.findAll();
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        List<Category> categories = categoryService.findAll();
+        List<CategoryResponse> categoryList = categories.stream()
+                .map(CategoryMapper::toCategoryResponse)
+                .toList();
+        return ResponseEntity.ok(categoryList);
     }
 
     @PostMapping
-    public Category saveCategory(@RequestBody Category category) {
-        return categoryService.saveCategory(category);
+    public ResponseEntity<CategoryResponse> saveCategory(@RequestBody CategoryRequest request) {
+        Category newCategory = CategoryMapper.toCategory(request);
+        Category savedCategory = categoryService.saveCategory(newCategory);
+        CategoryResponse categorySaved = CategoryMapper.toCategoryResponse(savedCategory);
+        return ResponseEntity.status(HttpStatus.CREATED).body(categorySaved);
     }
 
     @GetMapping("/{id}")
-    public Category getCategoryById(@PathVariable("id") Long id) {
-        return categoryService.getCategoryById(id).orElse(null);
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable("id") Long id) {
+        Optional<Category> optCategory = categoryService.getCategoryById(id);
+        if (optCategory.isPresent()) {
+            CategoryResponse categoryFound = CategoryMapper.toCategoryResponse(optCategory.get());
+            return ResponseEntity.ok(categoryFound);
+        }
+        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
-    public String deleteCategory(@PathVariable("id") Long id) {
-        return categoryService.deleteCategoryById(id);
+    public ResponseEntity<Void> deleteCategory(@PathVariable("id") Long id) {
+        categoryService.deleteCategoryById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
---
+++ src/main/java/com/movies/controller/request/CategoryRequest.java
@@ -0,0 +1,4 @@
+package com.movies.controller.request;
+
+public record CategoryRequest(String name) {
+}
