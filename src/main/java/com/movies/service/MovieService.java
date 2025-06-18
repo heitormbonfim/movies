e20c1327414692d57c16b16cfe9367e778cc751f
heitormbonfim <heitor.mb27@gmail.com>
2025-06-18 20:47:02 -0300

    bugfix: return streaming and category when movie is added

--- src/main/java/com/movies/service/MovieService.java
+++ src/main/java/com/movies/service/MovieService.java
@@ -1,22 +1,45 @@
 package com.movies.service;
 
+import com.movies.entity.Category;
 import com.movies.entity.Movie;
+import com.movies.entity.Streaming;
 import com.movies.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MovieService {
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie) {
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreamings(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll() {
         return movieRepository.findAll();
+    }
+
+    private List<Category> findCategories(List<Category> categories) {
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> {
+            categoryService.findById(category.getId()).ifPresent(categoriesFound::add);
+        });
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreamings(List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> {
+            streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add);
+        });
+        return streamingsFound;
     }
 }
