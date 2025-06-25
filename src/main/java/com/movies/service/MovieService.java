d9d857d156bce4c789463e3f3e572af8338dacd0
heitormbonfim <heitor.mb27@gmail.com>
2025-06-24 22:56:38 -0300

    feat: update movies

--- src/main/java/com/movies/service/MovieService.java
+++ src/main/java/com/movies/service/MovieService.java
@@ -32,6 +32,32 @@ public class MovieService {
         return movieRepository.findById(id);
     }
 
+    public Optional<Movie> update(Long movieId, Movie updateMovie) {
+        Optional<Movie> optMovie = movieRepository.findById(movieId);
+        if (optMovie.isPresent()) {
+            List<Category> categories = this.findCategories(updateMovie.getCategories());
+            List<Streaming> streamings = this.findStreamings(updateMovie.getStreamings());
+
+
+            Movie movie = optMovie.get();
+            movie.setTitle(updateMovie.getTitle());
+            movie.setDescription(updateMovie.getDescription());
+            movie.setReleaseDate(updateMovie.getReleaseDate());
+            movie.setRating(updateMovie.getRating());
+
+            movie.getCategories().clear();
+            movie.setCategories(categories);
+
+            movie.getStreamings().clear();
+            movie.setStreamings(streamings);
+
+            movieRepository.save(movie);
+            return Optional.of(movie);
+        }
+
+        return Optional.empty();
+    }
+
     private List<Category> findCategories(List<Category> categories) {
         List<Category> categoriesFound = new ArrayList<>();
         categories.forEach(category -> {
